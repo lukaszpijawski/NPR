eedf832e380364b91170fb057c766265c302c531
JakubKaliszewski <kontakt@jakubkaliszewski.pl>
2017-11-28 15:13:58 +0100

    Added ListOfYears

--- Elixir/app.R
+++ Elixir/app.R
@@ -1,11 +1,5 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#A
 
 library(shiny)
 
@@ -14,8 +8,6 @@ ui <- fluidPage(
    
    # Application title
    titlePanel("Old Faithful Geyser Data"),
-   textInput("text", h3("Text input"), 
-             value = str),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
@@ -39,8 +31,41 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-  myData = read.csv("data/2010.csv")
-  str = myData$Marzec[2]  #dostęp do jednego wiersza
+  
+  row_99_02 <- c("L_MB", "L_KDPW", "L_NBP", "L_razem",
+                 "W_MB","W_KDPW", "W_NBP", "W_razem",
+                 "S_MB", "S_KDPW", "S_NBP", "S_razem")
+  
+  row_03_12 <- c("L_MB","L_MB_LORO","L_MB_kraj", "L_KDPW", "L_NBP", "L_razem",
+                 "W_MB","W_MB_LORO","W_MB_kraj", "W_KDPW", "W_NBP", "W_razem",
+                 "S_MB","S_MB_LORO","S_MB_kraj", "S_KDPW", "S_NBP", "S_razem")
+  
+  row_13_17 <- c("L_MB","L_MB_LORO","L_MB_kraj", "L_KDPW", "L_NBP","L_NBP_LORO","L_NBP_kraj", "L_razem",
+                 "W_MB","W_MB_LORO","W_MB_kraj", "W_KDPW", "W_NBP","W_NBP_LORO","W_NBP_kraj", "W_razem",
+                 "S_MB","S_MB_LORO","S_MB_kraj", "S_KDPW", "S_NBP","S_NBP_LORO","S_NBP_kraj", "S_razem")
+  
+  ListOfYears <- c(read.csv("data/1999.csv",row.names = row_99_02),
+                  read.csv("data/2000.csv",row.names = row_99_02),
+                  read.csv("data/2001.csv",row.names = row_99_02),
+                  read.csv("data/2002.csv",row.names = row_99_02),
+                  read.csv("data/2003.csv",row.names = row_03_12),
+                  read.csv("data/2004.csv",row.names = row_03_12),
+                  read.csv("data/2005.csv",row.names = row_03_12),
+                  read.csv("data/2006.csv",row.names = row_03_12),
+                  read.csv("data/2007.csv",row.names = row_03_12),
+                  read.csv("data/2008.csv",row.names = row_03_12),
+                  read.csv("data/2009.csv",row.names = row_03_12),
+                  read.csv("data/2010.csv",row.names = row_03_12),
+                  read.csv("data/2011.csv",row.names = row_03_12),
+                  read.csv("data/2012.csv",row.names = row_03_12),
+                  read.csv("data/2013.csv",row.names = row_13_17),
+                  read.csv("data/2014.csv",row.names = row_13_17),
+                  read.csv("data/2015.csv",row.names = row_13_17),
+                  read.csv("data/2016.csv",row.names = row_13_17),
+                  read.csv("data/2017.csv",row.names = row_13_17))
+  
+  print(class(ListOfYears))
+  
   
   
   
