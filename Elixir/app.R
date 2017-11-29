9de7f4de6edbb12e533d48d4c79d56f924028a48
JakubKaliszewski <kontakt@jakubkaliszewski.pl>
2017-11-29 19:24:18 +0100

    Repaired List of years

--- Elixir/app.R
+++ Elixir/app.R
@@ -2,6 +2,7 @@
 #A
 
 library(shiny)
+library(plotly)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -64,20 +65,47 @@ server <- function(input, output) {
                   read.csv("data/2016.csv",row.names = row_13_17),
                   read.csv("data/2017.csv",row.names = row_13_17))
   
-  print(ListOfYears[[2]]["S_MB",]$Średnia)
+  
+  print(ListOfYears[2])
+  print(as.numeric(ListOfYears[[2]][1:12][12,]))
+  print(as.numeric(ListOfYears[[2]][1:12]["S_razem",]))
+  print(colnames(ListOfYears[[2]]))
+  
+  
+
+  
+  x <- c('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+  y1 <- c(20, 14, 25, 16, 18, 22, 19, 15, 12, 16, 14, 17)
+  y2 <- c(19, 14, 22, 14, 16, 19, 15, 14, 10, 12, 12, 16)
+  data <- data.frame(x, y1, y2)
+  
+  #The default order will be alphabetized unless specified as below:
+  data$x <- factor(data$x, levels = data[["x"]])
+  
+  p <- plot_ly(data, x = ~x, y = ~y1, type = 'bar', name = 'Primary Product', marker = list(color = 'rgb(49,130,189)')) %>%
+    add_trace(y = ~y2, name = 'Secondary Product', marker = list(color = 'rgb(204,204,204)')) %>%
+    layout(xaxis = list(title = "", tickangle = -45),
+           yaxis = list(title = ""),
+           margin = list(b = 100),
+           barmode = 'group')
+  
+  # Create a shareable link to your chart
+  # Set up API credentials: https://plot.ly/r/getting-started
+  chart_link = api_create(p, filename="bar/rotated")
+  chart_link
   
   
   
   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-}
+#    output$distPlot <- renderPlot({
+#       # generate bins based on input$bins from ui.R
+#       x    <- faithful[, 2] 
+#       bins <- seq(min(x), max(x), length.out = input$bins + 1)
+#       
+#       # draw the histogram with the specified number of bins
+#       hist(x, breaks = bins, col = 'darkgray', border = 'white')
+#    })
+# }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
